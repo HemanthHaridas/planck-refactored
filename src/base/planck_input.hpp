807f2d8e9ed23271c9230c2942aa5c0780e82d8c
Hemanth Haridas <hemanthhari23@gmail.com>
2025-06-13 10:35:46 -0600

    Finished Geometry Interface

--- src/base/planck_input.hpp
+++ src/base/planck_input.hpp
@@ -1,6 +1,141 @@
 #pragma once
 
+#include <string>
+#include <fstream>
+#include <tuple>
+#include <memory>
+#include <vector>
+#include <sstream>
+#include <Eigen/Core>
+
+#include "planck_interface.hpp"
+#include "planck_exceptions.hpp"
 namespace Planck::IO
 {
-    
+
+    class InputReader
+    {
+    private:
+        std::string _input;
+        std::shared_ptr<std::ifstream> _file;
+
+    public:
+        Planck::Interface::ControlInterface _control;
+        Planck::Interface::SetupInterface _setup;
+        Planck::Interface::GeometryInterface _geom;
+
+        explicit InputReader(const std::string &input) : _input(input), _file(std::make_shared<std::ifstream>(_input)), _control(build_control_interface()), _setup(build_setup_interface()), _geom(build_geometry_interface())
+        {
+            if (!_file || !_file->is_open())
+                throw Planck::Exceptions::IOException("Could not open input file: " + _input);
+        }
+
+    private:
+        std::string toLower(const std::string &parsedString)
+        {
+            std::string lowerString = parsedString; // Create a copy to preserve the original string
+            std::transform(lowerString.begin(), lowerString.end(), lowerString.begin(), ::tolower);
+            return lowerString; // Return the transformed string
+        }
+
+        bool stringToBool(const std::string &parsedString)
+        {
+            std::string upperStr = parsedString;
+            std::transform(upperStr.begin(), upperStr.end(), upperStr.begin(), ::toupper); // Convert to uppercase
+
+            if (upperStr == "ON")
+                return true; // "ON" maps to true
+            else if (upperStr == "OFF")
+                return false; // "OFF" maps to false
+            else
+                throw std::invalid_argument("Invalid string for boolean conversion."); // Handle unexpected input
+        }
+
+        Planck::Interface::ControlInterface build_control_interface()
+        {
+            auto [keys, values] = tokenize_input("CONTROL", _file);
+            return Planck::Interface::ControlInterface(keys, values);
+        }
+
+        Planck::Interface::SetupInterface build_setup_interface()
+        {
+            auto [keys, values] = tokenize_input("SETUP", _file);
+            return Planck::Interface::SetupInterface(keys, values);
+        }
+
+        Planck::Interface::GeometryInterface build_geometry_interface()
+        {
+            auto [keys, values] = tokenize_input("MOL_SPEC", _file);
+            auto [atoms, coords] = tokenize_coords(_file);
+            return Planck::Interface::GeometryInterface(keys, values, atoms, coords);
+        }
+
+        std::tuple<std::vector<std::string>, std::vector<std::string>> tokenize_input(const std::string &header, std::shared_ptr<std::ifstream> file_pointer)
+        {
+            if (!file_pointer || !file_pointer->is_open())
+                throw Planck::Exceptions::IOException("Input stream is unavailable");
+
+            std::vector<std::string> keys;
+            std::vector<std::string> values;
+
+            std::string line_;
+            while (std::getline(*file_pointer, line_))
+            {
+                if (line_.starts_with("[") && line_.ends_with("]"))
+                {
+                    std::string header_ = line_.substr(1, line_.length() - 2);
+                    if (header == header_)
+                    {
+                        std::string data_;
+                        while (std::getline(*file_pointer, data_))
+                        {
+                            if (data_.find("END_" + header_) != std::string::npos)
+                                return {keys, values};
+
+                            std::string _key, _value;
+                            std::stringstream _data_buffer(data_);
+                            _data_buffer >> _key >> _value;
+
+                            if (!_key.empty() && !_value.empty())
+                            {
+                                keys.emplace_back(_key);
+                                values.emplace_back(_value);
+                            }
+                        }
+                    }
+                }
+            }
+
+            throw Planck::Exceptions::IOException("Section [" + header + "] not found or incomplete");
+        }
+
+        std::tuple<std::vector<std::string>, std::vector<Eigen::Vector3f>> tokenize_coords(std::shared_ptr<std::ifstream> file_pointer)
+        {
+            std::string line_;
+            std::vector<std::string> atoms;
+            std::vector<Eigen::Vector3f> coords;
+
+            while (getline(*file_pointer, line_))
+            {
+                if (line_.starts_with("COORDS") == 0)
+                {
+                    std::string coords_, _atom;
+                    Eigen::Vector3f _coords;
+                    while (getline(*file_pointer, coords_))
+                    {
+                        if (coords_.find("END_COORDS") == std::string::npos)
+                            return {atoms, coords};
+
+                        std::stringstream _coord_buffer(coords_);
+                        _coord_buffer >> _atom >> _coords[0] >> _coords[1] >> _coords[2];
+
+                        atoms.push_back(_atom);
+                        coords.push_back(_coords);
+                    }
+                }
+            }
+
+            throw Planck::Exceptions::IOException("Section [COORDS] not found or incomplete");
+        }
+    };
 };
--- src/base/planck_interface.hpp
+++ src/base/planck_interface.hpp
@@ -4,8 +4,33 @@
 #include <string>
 #include <vector>
 
+#include "planck_geometry.hpp"
+
 namespace Planck::Interface
 {
+
+    namespace Defaults
+    {
+        const std::double_t ANGTOBOHR = 1.8897259886;
+        const std::uint64_t MAXSCF = 120;
+        const std::uint64_t MAXITER = 120;
+        const std::uint64_t DIIS_DIM = 10;
+        const std::double_t TOLSCF = 1.0E-14;
+        const std::double_t TOLERI = 1.0E-14;
+
+        const std::string DEFAULT_BASIS = "sto-3g";
+        const std::string DEFAULT_THEORY = "rhf";
+        const std::string DEFAULT_CALC = "energy";
+        const std::string DEFAULT_COORD = "ang";
+
+        const bool USE_DIIS = true;
+        const bool USE_SYMM = true;
+
+        // this is the maximum supported boysindex
+        // change this value only if you regenerate
+        // the lookup table for the boysfunction
+        const std::uint64_t MAXM = 60;
+    }
 
     class BaseInterface
     {
@@ -16,30 +41,31 @@ namespace Planck::Interface
         explicit BaseInterface(const std::unordered_map<std::string, std::string> &options) : _options(options) {}
 
         template <typename T>
-        T get_value(const std::string &key) const
+        T get_value(const std::string &key, const T &default_value) const
         {
             auto iterator_ = _options.find(key);
             if (iterator_ == _options.end())
-                throw std::out_of_range("Key " + key + " was not found in the options");
+                return default_value;
 
             std::istringstream buffer_(iterator_->second);
             T value;
             if (!(buffer_ >> value))
-                throw std::invalid_argument("Failed to convert " + value + " to target type") 
+                throw std::invalid_argument("Failed to convert " + value + " to target type");
             return value;
         }
 
         template <>
-        bool get_value(const std::string &key) const
+        bool get_value(const std::string &key, const bool &default_value) const
         {
             auto iterator_ = _options.find(key);
             if (iterator_ == _options.end())
-                throw std::out_of_range("Key " + key + " was not found in the options");
+                return default_value;
 
             const std::string value = iterator_->second;
-            if (value == "TRUE" || value == "True" || value == "true" || value == "1")
+            // std::ranges::transform(value, value.begin(), ::toupper);
+            if (value == "TRUE" || value == "1")
                 return true;
-            if (value == "FALSE" || value == "False" || value == "false" || value == "0")
+            if (value == "FALSE" || value == "0")
                 return false;
             throw std::invalid_argument("Invalid Boolean argument for " + key);
         }
@@ -54,19 +80,26 @@ namespace Planck::Interface
         std::string _theory;
         std::string _basis;
         std::string _coor_type;
+
         bool _use_diis;
         bool _use_symm;
 
+        bool _is_initialized_by_user = false;
+
     public:
+        SetupInterface() = default;
         explicit SetupInterface(const std::vector<std::string> &keys, const std::vector<std::string> &values) : BaseInterface(build_map(keys, values)) {}
 
         void set_parameters_from_input() override
         {
-            _calc_type = get_value<std::string>("CALC_TYPE");
-            _theory    = get_value<std::string>("THEORY");
-            _basis     = get_value<std::string>("BASIS");
-            _use_diis  = get_value<bool>("USE_DIIS");
-            _use_symm  = get_value<bool>("USE_SYMM");
+            _calc_type    = get_value<std::string>("CALC_TYPE", Defaults::DEFAULT_CALC);
+            _theory       = get_value<std::string>("THEORY", Defaults::DEFAULT_THEORY);
+            _basis        = get_value<std::string>("BASIS", Defaults::DEFAULT_BASIS);
+
+            _use_diis     = get_value<bool>("USE_DIIS", Defaults::USE_DIIS);
+            _use_symm     = get_value<bool>("USE_SYMM", Defaults::USE_SYMM);
+            
+            _is_initialized_by_user = true;
         }
 
     private:
@@ -84,16 +117,30 @@ namespace Planck::Interface
 
     class ControlInterface : public BaseInterface
     {
+    private:
+        std::uint64_t _max_scf;
+        std::uint64_t _max_iter;
+        std::uint64_t _diis_dim;
+
+        std::double_t _tol_scf;
+        std::double_t _tol_eri;
+
+        bool _is_initialized_by_user = false;
+
     public:
+        ControlInterface() = default;
         explicit ControlInterface(const std::vector<std::string> &keys, const std::vector<std::string> &values) : BaseInterface(build_map(keys, values)) {}
 
         void set_parameters_from_input() override
         {
-            _calc_type = get_value<std::string>("CALC_TYPE");
-            _theory    = get_value<std::string>("THEORY");
-            _basis     = get_value<std::string>("BASIS");
-            _use_diis  = get_value<bool>("USE_DIIS");
-            _use_symm  = get_value<bool>("USE_SYMM");
+            _max_scf  = get_value<std::uint64_t>("MAX_SCF", Defaults::MAXSCF);
+            _max_iter = get_value<std::uint64_t>("MAX_ITER", Defaults::MAXITER);
+            _diis_dim = get_value<std::uint64_t>("DIIS_DIM", Defaults::DIIS_DIM);
+
+            _tol_scf  = get_value<std::double_t>("TOL_SCF", Defaults::TOLSCF);
+            _tol_eri  = get_value<std::double_t>("TOL_ERI", Defaults::TOLERI);
+
+            _is_initialized_by_user = true;
         }
 
     private:
@@ -109,4 +156,31 @@ namespace Planck::Interface
         }
     };
 
+    class GeometryInterface : public BaseInterface
+    {
+    private:
+        std::uint64_t _multiplicity;
+        std::int64_t _charge;
+        
+        Planck::Geometry::Molecule _molecule;
+
+    public:
+        GeometryInterface() = default;
+        explicit GeometryInterface(const std::vector<std::string> &keys, const std::vector<std::string> &values, std::vector<std::string> &atoms, std::vector<Eigen::Vector3f> &coords) : BaseInterface(build_map(keys, values)), _molecule(atoms, coords) {
+            _multiplicity = get_value<std::uint64_t>("MULTI", 1);
+            _charge       = get_value<std::int64_t>("CHARGE", 0);
+        }
+
+    private:
+        static std::unordered_map<std::string, std::string> build_map(const std::vector<std::string> &keys, const std::vector<std::string> &values)
+        {
+            if (keys.size() != values.size())
+                throw std::invalid_argument("Mismatched number of keys and values");
+
+            std::unordered_map<std::string, std::string> result;
+            for (size_t i = 0; i < keys.size(); ++i)
+                result[keys[i]] = values[i];
+            return result;
+        }
+    };
 };
